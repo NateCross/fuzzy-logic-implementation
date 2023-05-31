86b32f1dbb2242a7458291a6acbd248f1cefca93
Nathan Angelo B. Cruz <jonathan.a.b.cross@gmail.com>
2023-06-01 03:18:58 +0800

    Followed knowledge base of problem

--- fuzzy_logic.py
+++ fuzzy_logic.py
@@ -12,62 +12,163 @@ if __name__ == "__main__":
     root = tkinter.Tk()
     root.wm_title("Fuzzy Logic")
 
-    level = ctrl.Antecedent(np.arange(0, 100, 1), 'Level')
-    rate_of_change = ctrl.Antecedent(
-        np.arange(-100, 100, 1), 
-        'Rate of Change',
+    level = ctrl.Antecedent(np.arange(0, 11, 1), 'Water Level')
+    water_temperature = ctrl.Antecedent(
+        np.arange(0, 136, 1), 
+        'Water Temperature',
     )
-    valve = ctrl.Consequent(np.arange(0, 100, 1), 'State of Valve')
+    heater_value = ctrl.Consequent(np.arange(0, 11, 1), 'Heater Value')
 
-    level.automf(names=[
-        "Very Low", 
-        "Low", 
-        "Medium", 
-        "High", 
-        "Very High",
-    ])
-    rate_of_change.automf(names=[
-        "Negative", 
-        "Neutral", 
-        "Positive",
-    ])
-    valve.automf(names=[
-        "Sudden Close",
-        "Open Slow",
-        "Open Medium",
-        "Open Fast",
-        "Sudden Open",
-    ])
+    level['Very Low'] = fuzz.trimf(
+        level.universe,
+        [0, 1, 2]
+    )
+    level['Low'] = fuzz.trimf(
+        level.universe,
+        [1.5, 3, 4.5],
+    )
+    level['Medium'] = fuzz.trimf(
+        level.universe,
+        [4, 5, 6.5],
+    )
+    level['High'] = fuzz.trimf(
+        level.universe,
+        [6, 7, 8.5],
+    )
+    level['Very High'] = fuzz.trimf(
+        level.universe,
+        [8, 9, 10],
+    )
+    water_temperature['Very Cool'] = fuzz.trimf(
+        water_temperature.universe,
+        [0, 15, 30]
+    )
+    water_temperature['Cool'] = fuzz.trimf(
+        water_temperature.universe,
+        [22.5, 45, 67.5],
+    )
+    water_temperature['Medium'] = fuzz.trimf(
+        water_temperature.universe,
+        [60, 75, 97.5],
+    )
+    water_temperature['Hot'] = fuzz.trimf(
+        water_temperature.universe,
+        [90, 105, 127.5],
+    )
+    water_temperature['Very Hot'] = fuzz.trimf(
+        water_temperature.universe,
+        [120, 135, 135],
+    )
+    heater_value['Very Little'] = fuzz.trimf(
+        heater_value.universe,
+        [0, 1, 2],
+    )
+    heater_value['Little'] = fuzz.trimf(
+        heater_value.universe,
+        [1.5, 3, 4.5],
+    )
+    heater_value['Medium'] = fuzz.trimf(
+        heater_value.universe,
+        [4, 5, 6.5],
+    )
+    heater_value['High'] = fuzz.trimf(
+        heater_value.universe,
+        [6, 7, 8.5],
+    )
+    heater_value['A Lot'] = fuzz.trimf(
+        heater_value.universe,
+        [8, 9, 10],
+    )
 
 # Fuzzy rules
     water_control_system = ctrl.ControlSystem([
         ctrl.Rule(
-            level['Very High'], 
-            valve['Sudden Close'],
+            level['Very Low'] & water_temperature['Very Cool'], 
+            heater_value['Medium'],
         ),
         ctrl.Rule(
-            level['High'] & rate_of_change['Positive'],
-            valve['Open Medium'],
+            level['Very Low'] & water_temperature['Cool'],
+            heater_value['Little'],
         ),
         ctrl.Rule(
-            level['High'] & rate_of_change['Neutral'],
-            valve['Sudden Close'],
+            level['Very Low'] & water_temperature['Medium'],
+            heater_value['Medium'],
         ),
         ctrl.Rule(
-            level['High'] & rate_of_change['Negative'],
-            valve['Sudden Close'],
+            level['Very Low'] & water_temperature['Hot'],
+            heater_value['Very Little'],
         ),
         ctrl.Rule(
-            level['Medium'],
-            valve['Open Medium'],
+            level['Low'] & water_temperature['Very Cool'],
+            heater_value['High'],
         ),
         ctrl.Rule(
-            level['Low'],
-            valve['Open Fast'],
+            level['Low'] & water_temperature['Cool'],
+            heater_value['Little'],
         ),
         ctrl.Rule(
-            level['Very Low'],
-            valve['Sudden Open'],
+            level['Low'] & water_temperature['Medium'],
+            heater_value['High'],
+        ),
+        ctrl.Rule(
+            level['Low'] & water_temperature['Hot'],
+            heater_value['Very Little'],
+        ),
+        ctrl.Rule(
+            level['Medium'] & water_temperature['Very Cool'],
+            heater_value['High'],
+        ),
+        ctrl.Rule(
+            level['Medium'] & water_temperature['Cool'],
+            heater_value['Medium'],
+        ),
+        ctrl.Rule(
+            level['Medium'] & water_temperature['Medium'],
+            heater_value['High'],
+        ),
+        ctrl.Rule(
+            level['Medium'] & water_temperature['Hot'],
+            heater_value['Little'],
+        ),
+        ctrl.Rule(
+            level['High'] & water_temperature['Very Cool'],
+            heater_value['A Lot'],
+        ),
+        ctrl.Rule(
+            level['High'] & water_temperature['Cool'],
+            heater_value['High'],
+        ),
+        ctrl.Rule(
+            level['High'] & water_temperature['Medium'],
+            heater_value['A Lot'],
+        ),
+        ctrl.Rule(
+            level['High'] & water_temperature['Hot'],
+            heater_value['Little'],
+        ),
+        ctrl.Rule(
+            level['High'] & water_temperature['Very Hot'],
+            heater_value['Very Little'],
+        ),
+        ctrl.Rule(
+            level['Very High'] & water_temperature['Very Cool'],
+            heater_value['A Lot'],
+        ),
+        ctrl.Rule(
+            level['Very High'] & water_temperature['Cool'],
+            heater_value['A Lot'],
+        ),
+        ctrl.Rule(
+            level['Very High'] & water_temperature['Medium'],
+            heater_value['A Lot'],
+        ),
+        ctrl.Rule(
+            level['Very High'] & water_temperature['Hot'],
+            heater_value['Medium'],
+        ),
+        ctrl.Rule(
+            level['Very High'] & water_temperature['Very Hot'],
+            heater_value['Very Little'],
         ),
     ])
 
@@ -75,8 +176,8 @@ if __name__ == "__main__":
 # there is a value
     water_control = ctrl.ControlSystemSimulation(water_control_system)
     water_control.inputs({
-        'Level': 0,
-        'Rate of Change': 0,
+        'Water Level': 0,
+        'Water Temperature': 0,
     })
     water_control.compute()
 
@@ -85,7 +186,7 @@ if __name__ == "__main__":
     output_var = tkinter.DoubleVar()
     output_formatted = tkinter.StringVar()
 
-    fig, _ = valve.get_view(sim=water_control)
+    fig, _ = heater_value.get_view(sim=water_control)
 
 # Define tkinter widgets
 
@@ -108,21 +209,21 @@ if __name__ == "__main__":
         global toolbar
 
         water_control.inputs({
-            'Level': level_var.get(),
-            'Rate of Change': rate_var.get(),
+            'Water Level': level_var.get(),
+            'Water Temperature': rate_var.get(),
         })
         water_control.compute()
-        output_var.set(water_control.output['State of Valve'])
+        if not 'Heater Value' in water_control.output: return
+        output_var.set(water_control.output['Heater Value'])
         output_formatted.set(f"{output_var.get():9.2f}")
 
         # So far, only way to make it refresh
         # due to the unique construction of this widget
         canvas.get_tk_widget().destroy()
-        fig, _ = valve.get_view(sim=water_control)
+        fig, _ = heater_value.get_view(sim=water_control)
         canvas = FigureCanvasTkAgg(fig, master=canvas_frame)
         canvas.draw()
 
-        # toolbar.update()
         toolbar.destroy()
         toolbar = NavigationToolbar2Tk(canvas, canvas_frame, pack_toolbar=False)
         toolbar.update()
@@ -135,6 +236,7 @@ if __name__ == "__main__":
             fill=tkinter.BOTH,
             expand=True,
         )
+
 
     toolbar.pack(
         side=tkinter.BOTTOM,
@@ -176,7 +278,7 @@ if __name__ == "__main__":
     level_label = tkinter.Label(
         level_frame,
         borderwidth=2,
-        text="Level",
+        text="Water Level",
     ).pack()
     level_entry = tkinter.Entry(
         level_frame,
@@ -188,39 +290,41 @@ if __name__ == "__main__":
         level_frame,
         variable=level_var,
         from_=0,
-        to=100,
+        to=10,
+        tickinterval=0.1,
         orient=tkinter.HORIZONTAL,
         showvalue=False,
     ).pack()
     for child in level_frame.winfo_children():
         child.pack_configure(padx=5, pady=5)
 
-    rate_frame = tkinter.Frame(
+    water_temperature_frame = tkinter.Frame(
         control_frame_buttons,
         relief=tkinter.GROOVE,
         pady=20,
         borderwidth=2,
     )
-    rate_frame.pack(pady=20)
-    rate_label = tkinter.Label(
-        rate_frame,
-        text="Rate of Change",
+    water_temperature_frame.pack(pady=20)
+    water_temperature_label = tkinter.Label(
+        water_temperature_frame,
+        text="Water Temperature",
     ).pack()
-    rate_entry = tkinter.Entry(
-        rate_frame,
+    water_temperature_entry = tkinter.Entry(
+        water_temperature_frame,
         textvariable=rate_var,
         width=4,
         justify=tkinter.CENTER,
     ).pack()
-    rate_slider = tkinter.Scale(
-        rate_frame,
+    water_temperature_slider = tkinter.Scale(
+        water_temperature_frame,
         variable=rate_var,
-        from_=-100,
-        to=100,
+        from_=0,
+        to=135,
+        tickinterval=0.1,
         orient=tkinter.HORIZONTAL,
         showvalue=False,
     ).pack()
-    for child in rate_frame.winfo_children():
+    for child in water_temperature_frame.winfo_children():
         child.pack_configure(padx=5, pady=5)
 
     output_frame = tkinter.Frame(
@@ -232,7 +336,7 @@ if __name__ == "__main__":
     output_frame.pack(pady=20)
     output_label = tkinter.Label(
         output_frame,
-        text="Output: State of Valve",
+        text="Output: Heater Value",
     ).pack()
     output_text = tkinter.Label(
         output_frame,
